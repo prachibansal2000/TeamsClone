e6a3577b8c18e425854bcb96323527d0f1d46f38
prachibansal2000 <prachibansal2111@gmail.com>
2021-06-16 01:52:22 +0530

    commit-3

--- src/Components/SideNavBar.js
+++ src/Components/SideNavBar.js
@@ -1,11 +1,44 @@
 import "../CSS/SideNavBar.css";
-import { BsBellFill } from "react-icons/bs";
+import { BsBellFill, BsThreeDots, BsChatDots } from "react-icons/bs";
+import { SiMicrosoftteams } from "react-icons/si";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { ImFilesEmpty } from "react-icons/im";
+import { AiTwotoneAppstore } from "react-icons/ai";
+import { BiHelpCircle } from "react-icons/bi";
 const sidenav = () => {
   return (
     <div class="sidebar">
-      <div className="icons">
+      <a>
+        <BsChatDots className="icons" />
+        Chat
+      </a>
+      <a>
+        <BsBellFill className="icons" />
+        Activity
+      </a>
+      <a>
+        <SiMicrosoftteams className="icons" />
+        Teams
+      </a>
+      <a>
+        <FaRegCalendarAlt className="icons" />
+        Calendar
+      </a>
+      <a>
+        <ImFilesEmpty className="icons" />
+        Files
+      </a>
+      <a>
+        <BsThreeDots className="icons" />
+      </a>
+      <div style={{ marginTop: "60px" }}>
         <a>
-          <BsBellFill />
+          <AiTwotoneAppstore className="icons" />
+          Apps
+        </a>
+        <a>
+          <BiHelpCircle className="icons" />
+          Help
         </a>
       </div>
     </div>
